e3240fa79856a5cb871abf448becc531136726fa
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-07 00:46:56 +0300

    a

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-"""floor which takes a float n as argument and returns the floor of the float."""
+"""
+floor which takes a float n as argument
+and returns the floor of the float."""
+
 
 import math
+
 
 def floor(n: float) -> int:
     """Return the floor of the float n."""
