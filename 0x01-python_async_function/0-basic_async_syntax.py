bfd4995e33de444ffe107908f7c3408f322f62e5
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-10 23:09:25 +0300

    documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """This is a coroutine function"""
     delay = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay
