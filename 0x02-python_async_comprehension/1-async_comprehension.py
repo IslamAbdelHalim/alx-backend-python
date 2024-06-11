8e07c755e6e53163643ffc0a821d05359fe3bfde
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-11 22:08:06 +0300

    a

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,8 +3,9 @@
 
 
 import asyncio
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension() -> List[float]:
     """. Async Comprehensions """
