190af52b2c8845c60770e03ac80eb67990f96c06
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-06-11 22:05:37 +0300

    a

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async Comprehensions """
+
+
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+from typing import List
+
+async def async_comprehension() -> List[float]:
+    """. Async Comprehensions """
+    return [i async for i in async_generator()]
