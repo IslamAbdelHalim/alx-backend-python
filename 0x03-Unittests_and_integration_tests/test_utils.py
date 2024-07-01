5b47b0e7e7ee9e12fd3dcaa1b6dabd8523351488
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-07-02 00:42:09 +0300

    a

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,9 @@ class TestAccessNestedMap(unittest.TestCase):
     """
 
     @parameterized.expand([
-        ({"a": 1},("a",), 1),
-        ({"a": {"b": 2}},("a",), {"b": 2}),
-        ({"a": {"b": 2}},("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, input_map, path, expected) -> None:
         self.assertEqual(access_nested_map(input_map, path), expected)
